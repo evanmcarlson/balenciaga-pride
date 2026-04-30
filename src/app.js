1adfdc04746480d2bdf20b2d1d96b024d009aa0a
evanmcarlson <emcarlson2@usfca.edu>
2026-04-30 14:57:40 -0700

    load image targets, remove unused external

--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
-// Copyright (c) 2022 8th Wall, Inc.
-//
-// app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
-// is loaded, and before body.html is loaded.
 import {chromaKeyShader} from './chromakey'
 AFRAME.registerShader('chromakey', chromaKeyShader)
 
@@ -19,3 +15,14 @@ AFRAME.registerComponent('play-video', {
     this.el.sceneEl.addEventListener('xrimagefound', playVideo)
   },
 })
+
+// load image target
+const onxrloaded = () => {
+  XR8.XrController.configure({
+    imageTargetData: [
+      require('../image-targets/shirt.json'),
+    ],
+  })
+}
+
+window.XR8 ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)
